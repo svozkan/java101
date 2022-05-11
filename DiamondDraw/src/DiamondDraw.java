d00ff0154da25646bacb201d52f6956b1ad21b14
Salih Volkan Özkan <s.volkanozkan@gmail.com>
2022-05-11 23:02:27 +0300

    DiamondDraw Initial Commit

---
+++ DiamondDraw/src/DiamondDraw.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class DiamondDraw {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Bir Sayi Giriniz :");
+        int n = input.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+            for (int j = 0; j < (n - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i + 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+        for (int i = n-1; i >= 0 ; i--) {
+            for (int j = (n - i); j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = (2 * i + 1); k >= 1; k--) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+}
